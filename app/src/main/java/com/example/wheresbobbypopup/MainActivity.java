807c48d740310a938119b0e2ce99defdf1a3cdc1
Carey Lai <lzh.carey@gmail.com>
2019-12-01 18:08:59 +0800

    attempt to integrate upvote sys into dialog popup. app crashes upon clicking on button. Using the btnComment to test out voting sys now

--- app/src/main/java/com/example/wheresbobbypopup/MainActivity.java
+++ app/src/main/java/com/example/wheresbobbypopup/MainActivity.java
@@ -11,23 +11,36 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     Dialog myDialog;
+    int likeCount = 0;
+    int dislikeCount = 0;
+    Button likeBtn;
+    Button dislikeBtn;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         myDialog = new Dialog(this);
     }
+
     public void ShowPopup(View v) {
         TextView txtclose;
         Button btnComment;
         myDialog.setContentView(R.layout.custompopup);
-        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose = (TextView) myDialog.findViewById(R.id.txtclose);
+        btnComment = (Button) myDialog.findViewById(R.id.btn_comment);
         txtclose.setText("X");
-        btnComment = (Button) myDialog.findViewById(R.id.btncomment);
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 myDialog.dismiss();
+            }
+        });
+        btnComment.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                likeCount += 1;
+                likeBtn.setText(String.valueOf(likeCount));
             }
         });
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
