fa1fe76e00f2814789db1fde8547b2c041df0832
Carey Lai <lzh.carey@gmail.com>
2019-12-02 14:12:15 +0800

    upvote sys done. left with comment sys and scroll view

--- app/src/main/java/com/example/wheresbobbypopup/MainActivity.java
+++ app/src/main/java/com/example/wheresbobbypopup/MainActivity.java
@@ -29,6 +29,8 @@ public class MainActivity extends AppCompatActivity {
         myDialog.setContentView(R.layout.custompopup);
         txtclose = myDialog.findViewById(R.id.txtclose);
         btnComment = myDialog.findViewById(R.id.btn_comment);
+        likeBtn = myDialog.findViewById(R.id.btn_like);
+        dislikeBtn = myDialog.findViewById(R.id.btn_dislike);
         txtclose.setText("X");
         txtclose.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -36,11 +38,18 @@ public class MainActivity extends AppCompatActivity {
                 myDialog.dismiss();
             }
         });
-        btnComment.setOnClickListener(new View.OnClickListener() {
+        likeBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 likeCount += 1;
-                btnComment.setText(String.valueOf(likeCount));
+                likeBtn.setText(String.valueOf(likeCount));
+            }
+        });
+        dislikeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                dislikeCount -= 1;
+                dislikeBtn.setText(String.valueOf(dislikeCount));
             }
         });
         myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
